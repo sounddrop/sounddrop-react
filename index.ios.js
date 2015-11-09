cf8e4bfe1f6f45f6a47b5d8be582eb435fd7ed34
Franziskarrr Wittleder <franziska.wittleder@gmail.com>
2015-11-09 20:49:06 +0100

    first mockup Ios

--- index.ios.js
+++ index.ios.js
@@ -3,51 +3,61 @@
  * https://github.com/facebook/react-native
  */
 'use strict';
-
+var Button = require('react-native-button');
 var React = require('react-native');
 var {
   AppRegistry,
   StyleSheet,
   Text,
+  Image,
   View,
 } = React;
+
 
 var SoundDrop = React.createClass({
   render: function() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Hello there!
-
+          GREAT TO HAVE YOU HERE 
+          AT SOUNDDROP
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Image source={require('./sounddrop_logo.png')} style={styles.logo}/>
+        <Button style={styles.button} onPress={this._handlePress}>
+          CREATE
+        </Button>
+        <Button style={styles.button} onPress={this._handlePress}>
+          LISTEN
+        </Button>
       </View>
     );
   }
 });
+
+
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#1A0923',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
+    color: '#FFF',
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  button: {
+    borderColor: '#696969',
+    borderRadius: 8,
+    borderWidth: 1,
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FF8300',
+    margin: 30,
   },
 });
 
