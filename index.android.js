4622b63ccbd65d2edd824ed313575791f47eed96
Duana Saskia <starkcoffee+github@gmail.com>
2016-04-11 20:55:01 +0200

    Refactor index.android.js to use Main component
    And Navigator which is the React Native navigation wrapper.

--- index.android.js
+++ index.android.js
@@ -1,50 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-
 var React = require('react-native');
+var Main = require('SoundDrop/app/components/main');
+
+window.React = React;
+
 var {
   AppRegistry,
   StyleSheet,
-  Image,
-  Text,
-  TouchableHighlight,
-  View,
+	Navigator
 } = React;
-
-var UIExplorerButton = React.createClass({
-  propTypes: {
-    onPress: React.PropTypes.func,
-  },
-  render: function() {
-    return (
-      <TouchableHighlight
-        onPress={this.props.onPress}
-        style={styles.button}
-        underlayColor="grey">
-        <Text>
-          Hi
-        </Text>
-      </TouchableHighlight>
-    );
-  },
-});
 
 var SoundDrop = React.createClass({
   render: function() {
     return (
-      <View style={styles.container}>
-        <Image source={require('image!sounddrop_logo_name')} style={styles.logo}/>
-        <Image source={require('image!sounddrop_logo')} style={styles.icon}/>
-        <Text style={styles.welcome}>
-          Welcome to SoundDrop!
-        </Text>
-        <UIExplorerButton/>
-      </View>
+      <Navigator
+        initialRoute={{ id: 'welcome'}}
+        renderScene={this.navigatorRenderScene}
+      />
     );
-  }
+  },
+  navigatorRenderScene: function(route, navigator) {
+		switch (route.id) {
+      case 'welcome':
+        return (<Main navigator={navigator}/>);
+    }
+	}
 });
 
 var styles = StyleSheet.create({
@@ -53,26 +32,6 @@ var styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#1A0923',
-  },
-  icon: {
-    marginTop: 50, 
-    marginBottom: 60,
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    color: 'white',
-    margin: 10,
-  },
-  button: {
-    borderColor: '#696969',
-    borderRadius: 8,
-    borderWidth: 1,
-    padding: 10,
-    margin: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#d3d3d3',
   }
 });
 
